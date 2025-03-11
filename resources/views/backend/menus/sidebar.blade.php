245fe01a985b70276a3b54c26ca0f30491fe03c5
mauriciorosalesues <mauricio.rosales2@ues.edu.sv>
2025-03-10 18:03:12 -0600

    Initial commit

--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -1,7 +1,7 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="#" class="brand-link">
-        <img src="{{ asset('images/logosantaana_blanco.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" >
         <span class="brand-text font-weight" style="color: white">PANEL DE CONTROL</span>
     </a>
 
