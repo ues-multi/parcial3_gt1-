f3213a11fbd749e16caa546f556c9bf3f8d96cce
mauriciorosalesues <mauricio.rosales2@ues.edu.sv>
2025-03-15 14:47:46 -0600

    Initial commit

--- app/Http/Controllers/Backend/Dashboard/DashboardController.php
+++ app/Http/Controllers/Backend/Dashboard/DashboardController.php
@@ -3,17 +3,37 @@
 namespace App\Http\Controllers\Backend\Dashboard;
 
 use App\Http\Controllers\Controller;
-use App\Models\Libros;
-use App\Models\NichoCobros;
-use App\Models\NichoMunicipal;
-use App\Models\NichoMunicipalDetalle;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
    public function vistaDashboard()
    {
-       return view('backend.admin.dashboard.vistadashboard');
+        //Ejemplo #1
+        $numero = rand(1, 10);
+        $resultado = $this->evaluarNumero($numero);
+
+        $valores = [1, 2, 3, 4, 5];
+        $suma = $this->sumarValores($valores);
+       return view('backend.admin.dashboard.vistadashboard', compact('numero', 'resultado', 'valores', 'suma'));
+   }
+   //Ejemplo #1
+   private function evaluarNumero(int $num): string
+   {
+       return match (true) {
+           $num < 4 => "Número bajo",
+           $num < 7 => "Número medio",
+           default => "Número alto",
+       };
+   }
+    //Ejemplo #1
+   private function sumarValores(array $valores): int
+   {
+       $suma = 0;
+       foreach ($valores as $valor) {
+           if ($valor == 3) continue; // Ignora el número 3
+           $suma += $valor;
+       }
+       return $suma;
    }
 }
